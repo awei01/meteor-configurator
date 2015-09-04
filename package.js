2236ef15242a93d09ee07a962f68d0fc4178a2e3
austin wei <awei01@gmail.com>
2015-09-04 21:23:30 +0000

    fixing package git url

--- package.js
+++ package.js
@@ -6,7 +6,7 @@ Package.describe({
   // Brief, one-line summary of the package.
   summary: 'Simple configuration set/get manager.',
   // URL to the Git repository containing the source code for this package.
-  git: 'https://github.com/awei01/meteor-configurator.git',
+  git: 'https://github.com/awei01/meteor-configurator',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: null
